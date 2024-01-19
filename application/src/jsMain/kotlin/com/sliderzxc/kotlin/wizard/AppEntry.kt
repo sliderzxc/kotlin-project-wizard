c7cacd4388aaca8d43b3fa3db5911528bfae3aaa
vadwwxz <vadwwxz@gmail.com>
2024-01-19 22:22:40 +0200

    optimize imports

--- application/src/jsMain/kotlin/com/sliderzxc/kotlin/wizard/AppEntry.kt
+++ application/src/jsMain/kotlin/com/sliderzxc/kotlin/wizard/AppEntry.kt
@@ -1,16 +1,13 @@
 package com.sliderzxc.kotlin.wizard
 
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.ui.modifiers.*
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.modifiers.minHeight
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.components.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.components.style.toModifier
-import com.varabyte.kobweb.silk.init.InitSilk
-import com.varabyte.kobweb.silk.init.InitSilkContext
-import com.varabyte.kobweb.silk.theme.colors.palette.button
-import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.vh
 
 @App
 @Composable
